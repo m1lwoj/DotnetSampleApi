0a9de197682abf822c67f3d4c30c1be0f586e00b
Milosz Wojarski <milwojarski@gmail.com>
2022-11-26 20:23:02 +0100

    Update Startup.cs

--- TourApi/Startup.cs
+++ TourApi/Startup.cs
@@ -22,7 +22,7 @@ namespace TourApi
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfigturation Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
